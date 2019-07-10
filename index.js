27afbb9cc905eabc8235822eeceac11e67781ce2
Dilip Kr. Shukla <contact@fusionstrings.com>
2019-07-10 22:40:01 +0200

    update browserslist

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = [
+	'cover 99.5% in my stats',
 	'> 1%',
 	'not android >= 0',
 	'not samsung >= 0',
 	'not ie >= 0',
 	'not op_mini all >= 0',
-	'not and_uc >= 0'
+	'not and_uc >= 0',
+	'not dead'
 ];
